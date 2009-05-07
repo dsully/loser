85973ba5eaa18783c1b0a6c434182089ba5cdee9
Dan Sully <daniel-github@electricrain.com>
2009-05-07 16:02:39 -0700

    Put anted in the db.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,6 @@
-# This is a default user class used to activate merb-auth.  Feel free to change from a User to
-# Some other class, or to remove it altogether.  If removed, merb-auth may not work by default.
-#
 # Don't forget that by default the salted_user mixin is used from merb-more
 # You'll need to setup your db as per the salted_user mixin, and you'll need
 # To use :password, and :password_confirmation when creating a user
-#
-# see merb/merb-auth/setup.rb to see how to disable the salted_user mixin
 #
 # You will need to setup your database and create a user.
 class User
@@ -14,6 +9,7 @@ class User
   property :id,     Serial
   property :login,  String, :index => true
   property :name,   String
+  property :anted,  Boolean, :default => false
 
   has n, :weighings, :order => [ :date ]
   belongs_to :round
