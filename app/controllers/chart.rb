65b9ec80806231fc52e5b83719491f789279e0c3
Dan Sully <daniel-github@electricrain.com>
2009-05-07 20:19:39 -0700

    Fix chart to start at 0 for everyone.

--- app/controllers/chart.rb
+++ app/controllers/chart.rb
@@ -5,12 +5,17 @@ class Chart < Application
     @data  = []
 
     @round.users.each do |u|
+      records = u.weighings.collect do |w|
+        # Javascript needs the timestamps in milliseconds.
+        [ Time.parse(w.date.to_s).to_i * 1000, (w.weight - u.start_weight).round_at(2) ]
+      end
+
+      # Start out at 0.
+      records.unshift [ (records.first.first - (1.day.to_i * 1000)), 0 ]
+
       @data << {
         "label" => u.name,
-        "data"  => u.weighings.collect do |w|
-          # Javascript needs the timestamps in milliseconds.
-          [ Time.parse(w.date.to_s).to_i * 1000, (w.weight - u.start_weight).round_at(2) ]
-        end
+        "data"  => records
       }
     end
 
