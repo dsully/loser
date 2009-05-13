b217e414ff06a7b85dfbbeae2a383a543fc1612b
Dan Sully <daniel-github@electricrain.com>
2009-05-13 11:55:27 -0700

    Initial profile page.

---
+++ app/controllers/profile.rb
@@ -0,0 +1,37 @@
+class Profile < Application
+
+  def index
+    render
+  end
+
+  def update
+    changed = false
+
+    unless params["password"].empty?
+      if params["password"] == params["password_confirm"]
+
+        session.user.password = session.user.password_confirmation = params["password"]
+        changed = true
+      else
+        return redirect "/profile", :message => { :notice => "Passwords don't match!" }
+      end
+    end
+
+    unless params["email"].empty?
+      if session.user.email != params["email"]
+        session.user.email = params["email"]
+        changed = true
+      end
+    end
+
+    if changed
+      unless session.user.save
+        return redirect "/profile", :message => { :notice => session.user.errors.values.join("\n") }
+      end
+
+      redirect "/profile", :message => { :notice => "Profile updated!" }
+    else
+      redirect "/profile", :message => { :notice => "No changes." }
+    end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,14 @@ class User
 
   property :id,     Serial
   property :login,  String, :index => true
-  property :email,  String, :index => true
   property :name,   String
   property :anted,  Boolean, :default => false
+
+  property :email,  String, :index => true, :nullable => false, :unique => true, :format => :email_address, :messages => {
+    :presence  => "We need your email address.",
+    :is_unique => "We already have that email.",
+    :format    => "Doesn't look like an email address to me ..."
+  }
 
   # Starting weight. Not sure if this is the right class for it.
   property :start,  Float
